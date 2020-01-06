0e891f3d68a1237ecc0e464c75493f1a8a47224e
imefGames <michel.pierre.35@gmail.com>
2020-01-06 22:28:10 +0100

    Implemented better frame handling.

--- CellularAutomaton/main.cpp
+++ CellularAutomaton/main.cpp
@@ -11,29 +11,41 @@ int main()
 
     bool keepRunning{ true };
     bool isAutomatonRunning{ false };
+    bool forceGridComputation{ false };
+
+    int stepGridComputationsPerSecond{ 2 };
+    std::chrono::microseconds gridComputationsPerSecond{ 100ms };
+    std::chrono::microseconds minGridComputationsPerSecond{ 12500us };
+    std::chrono::microseconds maxGridComputationsPerSecond{ 1600ms };
+
     CellularAutomaton::Grid cellularAutomatonGrid{ 1, 2, 50, 20 };
     CellularAutomaton::Renderer renderer;
     CellularAutomaton::InputHandler inputHandler;
 
+    auto lastGridComputationTime{ std::chrono::high_resolution_clock::now() };
     inputHandler.RegisterButtonPressCommand('q', [&keepRunning]() { keepRunning = false; });
     inputHandler.RegisterButtonPressCommand('r', [&cellularAutomatonGrid]() { cellularAutomatonGrid.RandomizeGrid(); });
     inputHandler.RegisterButtonPressCommand('c', [&cellularAutomatonGrid]() { cellularAutomatonGrid.ClearGrid(); });
-    inputHandler.RegisterButtonPressCommand('s', [&isAutomatonRunning, &cellularAutomatonGrid]() { if (!isAutomatonRunning) { cellularAutomatonGrid.ComputeNextGrid(); } });
+    inputHandler.RegisterButtonPressCommand('s', [&forceGridComputation]() { forceGridComputation = true; });
     inputHandler.RegisterButtonPressCommand(' ', [&isAutomatonRunning]() { isAutomatonRunning = !isAutomatonRunning; });
+    inputHandler.RegisterButtonPressCommand('+', [&gridComputationsPerSecond, maxGridComputationsPerSecond, stepGridComputationsPerSecond]() { if (gridComputationsPerSecond < maxGridComputationsPerSecond) { gridComputationsPerSecond *= stepGridComputationsPerSecond; } });
+    inputHandler.RegisterButtonPressCommand('-', [&gridComputationsPerSecond, minGridComputationsPerSecond, stepGridComputationsPerSecond]() { if (gridComputationsPerSecond > minGridComputationsPerSecond) { gridComputationsPerSecond /= stepGridComputationsPerSecond; } });
     inputHandler.RegisterMouseClickCommand([&cellularAutomatonGrid](unsigned int clickX, unsigned int clickY) { cellularAutomatonGrid.ToggleCellState(clickX, clickY); });
 
     while (keepRunning)
     {
         inputHandler.HandleEvents();
 
-        if (isAutomatonRunning)
+        auto currentTime{ std::chrono::high_resolution_clock::now() };
+        if (isAutomatonRunning && (forceGridComputation || std::chrono::duration_cast<std::chrono::microseconds>(currentTime - lastGridComputationTime) > gridComputationsPerSecond))
         {
             cellularAutomatonGrid.ComputeNextGrid();
+            lastGridComputationTime = std::chrono::high_resolution_clock::now();
+            forceGridComputation = false;
         }
 
         cellularAutomatonGrid.Render(renderer);
         renderer.RenderFrame();
-        std::this_thread::sleep_for(100ms);
     }
 
     return 0;
